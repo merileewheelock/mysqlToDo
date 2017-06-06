c1b69ff23a14b36b3d875dc5e929ce13bda55a34
merilee-wheelock <eelirem@gmail.com>
2017-06-06 10:45:18 -0400

    make edit button functional

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,10 @@ router.get('/', function(req, res, next) {
 	var message = req.query.msg;
 	if (message == "added"){
 		message = "Your task was added!";
+	}else if (message == "updated"){
+		message = "Your task was updated!";
+	}else if (message == "deleted"){
+		message = "Your task was deleted!";
 	}
 	var selectQuery = "SELECT * FROM tasks ORDER BY taskDate;";
 	connection.query(selectQuery, (error, results)=>{
@@ -39,9 +43,11 @@ router.post('/addItem',(req,res)=>{
 	var dueDate = req.body.newTaskDate;
 	// We know what they submitted from the form. It comes to this route inside req.body.NAMEOFFIELD.
 	// Now we need to insert it into MySQL.
-	var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES('"+newTask+"','"+dueDate+"')"
+	// var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES('"+newTask+"','"+dueDate+"')"
+	var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES(?,?)" // The ? are placeholders, called below [newTask, dueDate]
 	// res.send(insertQuery);
-	connection.query(insertQuery, (error, results)=>{
+	// connection.query(insertQuery, (error, results)=>{
+	connection.query(insertQuery, [newTask, dueDate], (error, results)=>{
 		if (error) throw error;
 		res.redirect('/?msg=added');
 	})
@@ -54,6 +60,30 @@ router.get('/delete/:id', (req, res)=>{
 		res.redirect('/?msg=deleted');
 	});
 	// res.send(idToDelete);
+});
+
+// Make a new route to handle the edit page. It will be /edit/IDTOEDIT
+router.get('/edit/:id', (req, res)=>{
+	var idToEdit = req.params.id;
+	var selectQuery = "SELECT * FROM tasks WHERE id = ?";
+	connection.query(selectQuery, [idToEdit], (error, results)=>{
+		// res.json(results);
+		res.render('edit',{
+			task: results[0]
+		});
+
+	});
+});
+
+router.post('/editItem', (req, res)=>{
+	// res.json(req.body);
+	var newTask = req.body.newTask;
+	var newTaskDate = req.body.newTaskDate;
+	var idToEdit = req.query.id;
+	var updateQuery = "UPDATE tasks SET taskName = ?, taskDate = ? WHERE id = ?"
+	connection.query(updateQuery, [newTask,newTaskDate,idToEdit], (error,results)=>{
+		res.redirect('/?msg=updated');
+	});
 });
 
 module.exports = router;
